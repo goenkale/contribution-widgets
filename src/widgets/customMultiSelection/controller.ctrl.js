43899118a3a8613d25d4d45b8b88b87f9cf74177
goenkale <victor.garcia-pereda@bonitasoft.com>
2017-10-11 12:06:23 +0200

    Added: spanish translation + label configuration

--- src/widgets/customMultiSelection/controller.ctrl.js
+++ src/widgets/customMultiSelection/controller.ctrl.js
@@ -7,20 +7,13 @@
  * You can leave the controller empty if you do not need it.
  **/
 function WidgetController($scope) {
-/*
-    var white = 'white';
 
-    // add a new variable in AngularJS scope. It'll be usable in the template directly with {{ backgroudColor }}
-    $scope.backgroudColor = white;
-
-    // define a function to be used in template with ctrl.toggleBackgroundColor()
-    this.toggleBackgroundColor = function () {
-        if ($scope.backgroudColor === white) {
-            // use the custom widget property backgroudColor with $scope.properties.backgroudColor
-            $scope.backgroudColor = $scope.properties.background;
-        } else {
-            $scope.backgroudColor = white;
-        }
+    $scope.localLang = {
+        selectAll       : "Seleccionar todos",
+        selectNone      : "Seleccionar ninguno",
+        reset           : "Deshacer",
+        search          : "Buscar...",
+        nothingSelected : "Nada seleccionado"         //default-label is deprecated and replaced with this.
     };
-    */
+
 }
